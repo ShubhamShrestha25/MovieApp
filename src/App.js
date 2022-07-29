677a99f89716ab1ce4f7f26b3dfef5b13aaa2dad
ShubhamShrestha25 <shubhamshrestha25@gmail.com>
2022-07-29 13:49:12 +0545

    Api configure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-
-import './App.css';
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
- 
-    </div>
-  );
+  return <div className="App">App</div>;
 }
 
 export default App;
