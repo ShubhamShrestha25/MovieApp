e749a8170b4da8ff19492503feb459252a454526
ShubhamShrestha25 <shubhamshrestha25@gmail.com>
2022-09-06 10:02:22 +0545

    Movie Details page complete

--- src/config/Routess.js
+++ src/config/Routess.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Catalog from "../pages/Catalog";
-import Details from "../pages/Details";
+import Detail from "../pages/detail/Detail";
 import Home from "../pages/Home";
 
 const Routess = () => {
   return (
     <Switch>
       <Route path="/:category/search/:keyword" component={Catalog} />
-      <Route path="/:category/:id" component={Details} />
+      <Route path="/:category/:id" component={Detail} />
       <Route path="/:category" component={Catalog} />
       <Route path="/" exact component={Home} />
     </Switch>
